1bb50b799cef7187fa72afefb0c159ca379d15e6
CV Productions <cveloza3@me.com>
2020-06-13 20:32:51 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,10 @@
 var express = require('express');
-// var https = require('https');
 var fs = require('fs');
 var http = require('http');
 var path = require('path');
 var bodyparser = require('body-parser');
 var app = express();
 var server = http.Server(app);
-// var server = https.createServer({
-   // key: fs.readFileSync('./key/key.pem'),
-  //  cert: fs.readFileSync('./key/cert.pem')
-// }, app);
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
